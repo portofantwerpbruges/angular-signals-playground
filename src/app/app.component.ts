37bca47e82478e59ffecf410c1c16325e02cc8f9
Florian Spier <florian.spier@portofantwerpbruges.com>
2024-12-13 14:08:28 +0100

    Signal Basics

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, computed, effect, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -8,5 +8,33 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'ng19-signals';
+  mySignal = signal(42); // == BehaviorSubject(42)
+
+  counter1 = signal(1);
+  counter2 = signal(2);
+  sum = computed(() => this.counter1() + this.counter2()) // == combineLatest
+
+  myValue = signal(42)
+
+  constructor() {
+    effect(() => {
+      console.log('sum updated', this.sum())
+    }); // == subscribe to sum Observable; do something when there is a new emission
+
+    effect(() => {
+      console.log('value updated', this.myValue())
+    });
+  }
+
+  incrementCounter1(): void {
+    this.counter1.update(v => v + 1) // == BehaviorSubject.next
+  }
+
+  incrementCounter2(): void {
+    this.counter2.update(v => v + 1) // == BehaviorSubject.next
+  }
+
+  setValue() {
+    this.myValue.set(42)
+  }
 }
